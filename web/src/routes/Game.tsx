b1aa42405aafd124ee6ca4b37a84231858ccafad
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-28 00:41:13 +0300

    When going into the game route, start game if not started

--- web/src/routes/Game.tsx
+++ web/src/routes/Game.tsx
@@ -4,15 +4,10 @@ import Map from "@/components/_game/Map";
 import { useEffect } from "react";
 import { useGameStore } from "@/stores/gameStore";
 import { game } from "@core/game";
-import { CountryId } from "@core/types/country_id";
 
 export default function Game() {
   useEffect(() => {
     useGameStore.setState(s => {
-      s.data = game.createGameData(Date.now());
-      game.play.addCountry(s.data, { country: CountryId.Green });
-      game.play.addCountry(s.data, { country: CountryId.Red });
-      game.play.generate(s.data, { w: 10, h: 10 });
       game.play.start(s.data, {});
       s.country = game.util.turnTypeToCountry(s.data, s.data.turn.type);
     });
