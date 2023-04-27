43acf02bbf3571a7dd419e6f9b428a9de12d7dd7
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-28 00:41:25 +0300

    Update appStore.ts

--- web/src/stores/appStore.ts
+++ web/src/stores/appStore.ts
@@ -27,10 +27,10 @@ const initialState: AppStoreState = {
   route: "any",
 
   players: [
-    { id: "player", name: "Player", country: CountryId.Green, isAdmin: true },
-    { id: "", name: "", country: CountryId.Purple },
-    { id: "", name: "", country: CountryId.Red },
-    { id: "", name: "", country: CountryId.Yellow },
+    { id: undefined, name: undefined, country: CountryId.Green },
+    { id: undefined, name: undefined, country: CountryId.Purple },
+    { id: undefined, name: undefined, country: CountryId.Red },
+    { id: undefined, name: undefined, country: CountryId.Yellow },
   ],
   lobby: {
     owner: true,
