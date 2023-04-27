61bb29e51cc770a2507b4c220c682ec8b64e235c
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-27 20:07:27 +0300

    More lobby UI improvements

--- web/src/routes/Lobby.tsx
+++ web/src/routes/Lobby.tsx
@@ -1,13 +1,21 @@
-import { ActionIcon, Button, Flex, NumberInput, Text, Title } from "@mantine/core";
+import { ActionIcon, Button, Card, Divider, Flex, NumberInput, Text, Title } from "@mantine/core";
 import {
   IconArrowBigLeftFilled,
   IconArrowBigRightFilled,
   IconArrowRight,
+  IconBan,
   IconCopy,
   IconDeviceFloppy,
+  IconLogin,
+  IconLogout,
   IconMessageCircle2,
-  IconSettings
+  IconRobot,
+  IconSettings,
+  IconStarFilled
 } from "@tabler/icons-react";
+
+import UnitGreen from "@/assets/units/green.png";
+import { CountryId } from "@core/types/country_id";
 
 export default function Lobby() {
   return (
@@ -50,28 +58,70 @@ export default function Lobby() {
         </ActionIcon>
       </Flex>
 
-      <Flex>
-        player stuff
+      <Players />
+      <Map />
+    </Flex>
+  )
+}
+
+function Players() {
+  return (
+    <Flex direction="column" gap="xs">
+      <Player
+        country={CountryId.Green}
+        name={"Berk Cambaz"}
+        isAdmin
+      />
+      <Player
+        country={CountryId.Green}
+        name={"Berk Cambaz"}
+      />
+    </Flex>
+  )
+}
+
+interface PlayerProps {
+  country: CountryId;
+  name: string;
+
+  isAdmin?: boolean;
+}
+
+function Player({ country, name, isAdmin }: PlayerProps) {
+  return (
+    <Flex gap="md" justify="space-between">
+      <Flex gap="md">
+        <img src={UnitGreen} width={48} />
+        <Text>{name}</Text>
+      </Flex>
+      <Flex align="center" justify="flex-end" gap="xs">
+        <ActionIcon size={24}><IconBan /></ActionIcon>
+        <ActionIcon size={24}><IconLogin /></ActionIcon>
+        <ActionIcon size={24}><IconLogout /></ActionIcon>
+        {isAdmin && <IconStarFilled />}
+      </Flex>
+    </Flex>
+  )
+}
+
+function Map() {
+  return (
+    <Flex direction="column" gap="md">
+      <Title order={4}>Map Settings</Title>
+
+      <Flex align="center" justify="space-between" gap="md">
+        Width: <NumberInput />
       </Flex>
 
-      <Flex direction="column" gap="md">
-        <Title order={4}>Map Settings</Title>
-
-        <Flex align="center" justify="space-between" gap="md">
-          Width: <NumberInput />
-        </Flex>
-
-        <Flex align="center" justify="space-between" gap="md">
-          Height: <NumberInput />
-        </Flex>
-
-        <Flex align="center" justify="space-between" gap="md">
-          Seed: <NumberInput />
-        </Flex>
-
-        <Button>Preview</Button>
+      <Flex align="center" justify="space-between" gap="md">
+        Height: <NumberInput />
       </Flex>
 
+      <Flex align="center" justify="space-between" gap="md">
+        Seed: <NumberInput />
+      </Flex>
+
+      <Button>Preview</Button>
     </Flex>
   )
 }
