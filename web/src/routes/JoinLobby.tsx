f6dabd32c0dc986cf325a8a796279342b38b1b36
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-28 16:11:26 +0300

    Fix typo in JoinLobby route

--- web/src/routes/JoinLobby.tsx
+++ web/src/routes/JoinLobby.tsx
@@ -3,7 +3,7 @@ import { Button, Flex, TextInput } from "@mantine/core";
 export default function JoinLobby() {
   return (
     <Flex direction="column" align="center" gap="md">
-      <TextInput placeholder="Room ID..." />
+      <TextInput placeholder="Lobby ID..." />
       <Button>Join</Button>
     </Flex>
   )
