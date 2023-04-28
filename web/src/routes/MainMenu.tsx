9940d490b0b1cc3a588c62b38b7218be98c7fb6f
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-28 16:11:40 +0300

    Add Dorkodu logo to main menu

--- web/src/routes/MainMenu.tsx
+++ web/src/routes/MainMenu.tsx
@@ -1,8 +1,9 @@
 import { util } from "@/lib/util";
 import { useAppStore } from "@/stores/appStore";
 import { CountryId } from "@core/types/country_id";
-import { Button, Flex, Title } from "@mantine/core";
+import { Button, Flex, Image, Text, Title } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
+import DorkoduLogo from "@/assets/dorkodu_logo.svg";
 
 export default function MainMenu() {
   const navigate = useNavigate();
@@ -20,12 +21,20 @@ export default function MainMenu() {
     <div style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }}>
       <Flex direction="column" align="center" justify="center" gap="md" p="md" style={{ height: "100%" }}>
 
-        <Title align="center">Lords and Lands</Title>
+        <Flex direction="column" align="center">
+          <Title align="center">Lords and Lands</Title>
+          <Text size="xs" weight="bold" color="dimmed">v0.1.0</Text>
+        </Flex>
 
-        <Flex direction="column" gap="md" style={{ maxWidth: 360, width: "100%" }}>
+        <Flex direction="column" my="md" gap="md" style={{ maxWidth: 240, width: "100%" }}>
           <Button onClick={createLobby}>Create Lobby</Button>
           <Button onClick={joinLobby}>Join Lobby</Button>
           <Button onClick={settings}>Settings</Button>
+        </Flex>
+
+        <Flex direction="column" align="center" gap="xs">
+          <Image src={DorkoduLogo} maw={240} />
+          <Text color="dimmed"><b>Dorkodu</b> &copy; {new Date().getFullYear()}</Text>
         </Flex>
 
       </Flex>
