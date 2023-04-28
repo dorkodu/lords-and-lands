e4dbb0ac4e28726ce21fcb27335b268370484e83
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-28 15:41:13 +0300

    Fix bug in remove country action

--- core/src/actions/remove_country.ts
+++ core/src/actions/remove_country.ts
@@ -4,9 +4,9 @@ import { CountryId } from "../types/country_id";
 type Info = { country: CountryId };
 
 export function removeCountryActable(data: IGameData, info: Info): boolean {
-  // If country already exist
+  // If country doesn't exist
   const existing = data.countries.filter(c => c.id === info.country).length > 0;
-  if (existing) return false;
+  if (!existing) return false;
 
   return true;
 }
