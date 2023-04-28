f213aa6bda91ec953cb1ed30e1ffe0b8a6715e07
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-28 09:59:51 +0300

    Remember map position & scale

--- web/src/components/_game/Map.tsx
+++ web/src/components/_game/Map.tsx
@@ -1,6 +1,7 @@
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch"
 import { useLayoutEffect, useState } from "react";
 import Tilemap from "./Tilemap";
+import { useGameStore } from "@/stores/gameStore";
 
 export default function Map() {
   const [size, setSize] = useState({ x: innerWidth, y: innerHeight });
@@ -10,14 +11,20 @@ export default function Map() {
     window.addEventListener("resize", onResize);
     return () => window.removeEventListener("resize", onResize);
   }, []);
-
+  console.log(useGameStore.getState().map.positionX)
   return (
     <TransformWrapper
+      initialPositionX={useGameStore.getState().map.positionX}
+      initialPositionY={useGameStore.getState().map.positionY}
+      initialScale={useGameStore.getState().map.scale}
+
       maxScale={1}
       minScale={0.25}
-      centerOnInit={true}
+
       limitToBounds={false}
       doubleClick={{ disabled: true }}
+
+      onTransformed={(_ref, state) => { useGameStore.setState(s => { s.map = state }) }}
     >
       <TransformComponent wrapperStyle={{ width: `${size.x}px`, height: `${size.y}px` }}>
         <Tilemap />
--- web/src/stores/gameStore.ts
+++ web/src/stores/gameStore.ts
@@ -13,6 +13,8 @@ export interface GameStoreState {
   hoveredTile: ITile | undefined;
   selectedUnitTile: ITile | undefined;
   moveableTiles: ITile[];
+
+  map: { positionX: number, positionY: number, scale: number };
 }
 
 export interface GameStoreAction {
@@ -26,6 +28,8 @@ const initialState: GameStoreState = {
   hoveredTile: undefined,
   selectedUnitTile: undefined,
   moveableTiles: [],
+
+  map: { positionX: 0, positionY: 0, scale: 1 },
 }
 
 export const useGameStore = create(
