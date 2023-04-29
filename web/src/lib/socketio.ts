00ba5c12283508595ef2b5f61ea7f8caf019eb09
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-29 12:17:47 +0300

    Base setup for web's socket.io

--- web/src/lib/socketio.ts
+++ web/src/lib/socketio.ts
@@ -2,4 +2,39 @@ import { io, Socket } from "socket.io-client";
 import type { ServerToClientEvents, ClientToServerEvents } from "@api/websocket/types";
 
 export const socketio: Socket<ServerToClientEvents, ClientToServerEvents> = io();
-socketio.emit("join-lobby", { id: "tet" }, (status) => { });
+
+// Default events \\
+socketio.on("connect", () => {
+  console.log("connect")
+});
+
+socketio.on("connect_error", (_err) => {
+  console.log("connect_error")
+});
+
+socketio.on("disconnect", (_reason, _description) => {
+  console.log("disconnect")
+});
+// Default events \\
+
+// Game events \\
+socketio.on("server-create-lobby", (data) => {
+
+});
+
+socketio.on("server-join-lobby", (data) => {
+
+});
+
+socketio.on("server-leave-lobby", (data) => {
+
+});
+
+socketio.on("server-lobby-update", (data) => {
+
+});
+
+socketio.on("server-change-country", (data) => {
+
+});
+// Game events \\
