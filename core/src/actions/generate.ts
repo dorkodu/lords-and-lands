e71b1b578bb060ce3f9b7bd913054701c70e0106
Berk Cambaz <berkcambaz12321@gmail.com>
2023-05-01 01:00:22 +0300

    Disable map generation when there are less than 2 not none countries

--- core/src/actions/generate.ts
+++ core/src/actions/generate.ts
@@ -1,6 +1,7 @@
 import { IGameData } from "../gamedata";
 import { tilemap } from "../lib/tilemap";
 import { ActionId } from "../types/action_id";
+import { CountryId } from "../types/country_id";
 
 type Info = { w: number, h: number, seed: number };
 export type IActionGenerate = { id: ActionId.Generate, info: Info };
@@ -8,6 +9,9 @@ export type IActionGenerate = { id: ActionId.Generate, info: Info };
 export function generateActable(data: IGameData, _info: Info): boolean {
   // If already started
   if (data.running) return false;
+
+  // If less than 2 non-none countries exist
+  if (data.countries.filter(c => c.id !== CountryId.None).length < 2) return false;
 
   return true;
 }
