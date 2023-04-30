5376c98a208c0c2f576be46e006da1464bb64d41
Berk Cambaz <berkcambaz12321@gmail.com>
2023-05-01 00:06:11 +0300

    Update Lobby.tsx

--- web/src/routes/Lobby.tsx
+++ web/src/routes/Lobby.tsx
@@ -97,7 +97,7 @@ export default function Lobby() {
             </ActionIcon>
           </Flex>
         </Flex> :
-        <Text>&bull; lobby offline &bull;</Text>
+        <Text>&bull; Lobby Offline &bull;</Text>
       }
 
       <Flex gap="md">
