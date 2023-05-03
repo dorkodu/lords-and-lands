e845079a2c2e096ee273f96a3081c13feafb8498
Berk Cambaz <berkcambaz12321@gmail.com>
2023-05-03 16:48:43 +0300

    Add subscription text & fix main menu styling

--- web/src/routes/MainMenu.tsx
+++ web/src/routes/MainMenu.tsx
@@ -1,5 +1,5 @@
 import { useAppStore } from "@/stores/appStore";
-import { Button, Flex, Image, Indicator, Text, Title } from "@mantine/core";
+import { Button, Flex, Image, Indicator, Text, Title, Card, Anchor } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 import DorkoduLogo from "@/assets/dorkodu_logo.svg";
 import { util } from "@/lib/util";
@@ -17,20 +17,23 @@ export default function MainMenu() {
 
   return (
     <div style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }}>
-      <Flex direction="column" align="center" justify="center" gap="md" p="md" style={{ minHeight: "100%" }}>
+      <Flex
+        direction="column" justify="center" gap="md" p="md" maw={360}
+        style={{ width: "100%", margin: "0 auto", minHeight: "100%" }}
+      >
 
         <Flex direction="column" align="center">
           <Title align="center">Lords and Lands</Title>
           <Text size="xs" weight="bold" color="dimmed">{util.version(3, 5, 2023)}</Text>
         </Flex>
 
-        <Flex direction="column" gap="md" style={{ maxWidth: 240, width: "100%" }}>
+        <Flex direction="column" gap="md" style={{ maxWidth: 240, width: "100%", margin: "0 auto" }}>
           <Button onClick={createLobby}>Create Lobby</Button>
           <Button onClick={joinLobby}>Join Lobby</Button>
           <Button onClick={settings}>Settings</Button>
         </Flex>
 
-        <Flex>
+        <Flex justify="center">
           <Indicator color={status ? "green" : "red"} position="middle-start" offset={-10}>
             <Text color={status ? "green" : "red"}>{status ? "Online" : "Offline"}</Text>
           </Indicator>
@@ -40,6 +43,16 @@ export default function MainMenu() {
           <Image src={DorkoduLogo} maw={240} />
           <Text color="dimmed"><b>Dorkodu</b> &copy; {new Date().getFullYear()}</Text>
         </Flex>
+
+        <Card withBorder>
+          <Flex direction="column" gap="xs">
+            <Text align="center">This is Lords and Lands early access.</Text>
+            <Text align="center">Get a 50% discounted subscription for $3/mo.</Text>
+            <Anchor href="https://buy.stripe.com/00gdS8cEPbU85bO7ss" target="_blank">
+              <Button fullWidth>Subscribe for 50% discount</Button>
+            </Anchor>
+          </Flex>
+        </Card>
 
       </Flex>
     </div>
