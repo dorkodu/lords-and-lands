da8c7e3ee45ab17141d1ab2c797f2a41b20f6254
Berk Cambaz <berkcambaz12321@gmail.com>
2023-05-03 12:15:30 +0300

    Update config.ts

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,6 +1,6 @@
 import { util } from "./lib/util";
 
-const port = util.parseNumber(process.env.PORT) || 9000;
+const port = util.parseNumber(process.env.PORT) || 8009;
 const env: "development" | "production" = (
   process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production"
 ) && process.env.NODE_ENV || "development";
